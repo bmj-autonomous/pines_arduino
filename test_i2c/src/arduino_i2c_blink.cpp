36baa13a861c270d632cb0da392924c60870f898
MarcusJones <jones.0bj3@gmail.com>
2019-07-14 15:11:43 +0200

    update i2c blink

--- test_i2c/src/arduino_i2c_blink.cpp
+++ test_i2c/src/arduino_i2c_blink.cpp
@@ -1,31 +1,25 @@
-// Wire Slave Receiver
-// by Nicholas Zambetti <http://www.zambetti.com>
+// I2C testing 
+// Recieve any characters, write them to 2 pins
+// Use for blinking both the internal LED, and an external LED
+// adapted M. Jones 2019
+//
+// Modified from: Wire Slave Receiver by Nicholas Zambetti <http://www.zambetti.com>
 
-// Demonstrates use of the Wire library
-// Receives data as an I2C/TWI slave device
-// Refer to the "Wire Master Writer" example for use with this
-
-// Created 29 March 2006
-
-// This example code is in the public domain.
-
-// 04-Feb-2018 mcarter adapted
 #include <Wire.h>
 #include <Arduino.h>
 
+const int kLedBuiltin = LED_BUILTIN; // onboard LED
 const int kLedPin = 12; // onboard LED
-const int k_I2C_Address = 0x8 // Specify the I2C address here
-const h
-// static_assert(LOW == 0, "Expecting LOW to be 0");
+const int k_I2C_Address = 0x8; // Specify the I2C address here
 void receiveEvent(int howMany);
 
-
 void setup() {
-  static_assert(LOW == 0, "Expecting LOW to be 0");
   Wire.begin(k_I2C_Address); // join i2c 
   Wire.onReceive(receiveEvent); // register event
   pinMode(kLedPin, OUTPUT);
+  pinMode(kLedBuiltin, OUTPUT);
   digitalWrite(kLedPin, LOW); // turn it off
+  digitalWrite(kLedBuiltin, LOW); // turn it off
 }
 
 void loop() {
@@ -37,6 +31,7 @@ void receiveEvent(int howMany) {
   while (Wire.available()) { // loop through all but the last
     char c = Wire.read(); // receive byte as a character
     digitalWrite(kLedPin, c);
+    digitalWrite(kLedBuiltin, c);
   }
 }
 
