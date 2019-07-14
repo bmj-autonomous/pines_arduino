dd2cca11586d468d8a3071167ddea8a9e0e8ae4f
MarcusJones <jones.0bj3@gmail.com>
2019-07-14 21:39:09 +0200

    refactor simple LCD script

--- python_lcd_driver/test_lcd.py
+++ python_lcd_driver/test_lcd.py
@@ -1,12 +1,15 @@
 import lcddriver 
 import time
 
-lcd = lcddriver.lcd()
+I2C_BUS_DEVICE_NUMBER = 1
+I2C_ADDRESS = 26
+
+lcd = lcddriver.lcd(I2C_ADDRESS,I2C_BUS_DEVICE_NUMBER)
 
 lcd.lcd_clear()
 
-lcd.lcd_display_string("Test l1", 1)
-lcd.lcd_display_string("Test l2", 2)
+lcd.lcd_display_string("Testing l1", 1)
+lcd.lcd_display_string("Testing l2", 2)
 
 time.sleep(5)
 
