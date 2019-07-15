de09e7b610ca630f7b54e91d8f120195a954e666
astro <jones.0bj3@gmail.com>
2019-07-15 21:38:54 +0200

    update LCD testing script

--- python_lcd_driver/test_lcd.py
+++ python_lcd_driver/test_lcd.py
@@ -1,16 +1,21 @@
 import lcddriver 
 import time
 
-I2C_BUS_DEVICE_NUMBER = 1
-I2C_ADDRESS = 0x26
+#I2C_BUS_DEVICE_NUMBER = 1
+#I2C_ADDRESS = 0x26
 
-lcd = lcddriver.lcd(I2C_ADDRESS,I2C_BUS_DEVICE_NUMBER)
+lcd1 = lcddriver.lcd(0x27,1)
+lcd2 = lcddriver.lcd(0x26,1)
 
-lcd.lcd_clear()
+lcd1.lcd_clear()
+lcd2.lcd_clear()
 
-lcd.lcd_display_string("Testing l1", 1)
-lcd.lcd_display_string("Testing l2", 2)
+lcd1.lcd_display_string("Testing LCD1 A", 1)
+lcd1.lcd_display_string("Testing LCD1 B", 2)
 
-time.sleep(5)
+lcd2.lcd_display_string("Testing LCD2 A", 1)
+lcd2.lcd_display_string("Testing LCD2 B", 2)
 
-lcd.lcd_backlight("off")
+#time.sleep(5)
+
+#lcd2.lcd_backlight("off")
